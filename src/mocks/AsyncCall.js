5771c497b4194025228679689fc1cf9d4209bd20
alberto <ariesco@leadtech.com>
2020-05-15 10:30:40 +0200

    change mock time

--- src/mocks/AsyncCall.js
+++ src/mocks/AsyncCall.js
@@ -1,9 +1,9 @@
 export class AsyncCall {
     customAction({ position }) {
         return new Promise(function(resolve) {
-            const milliSeconds = Math.floor(Math.random() * 4) * 100;
+            const milliSeconds = Math.floor(Math.random() * 9) * 100;
             console.log('position', position);
-            console.log('seconds', milliSeconds);
+            console.log('milliSeconds', milliSeconds);
             setTimeout(() => resolve(position), milliSeconds);
         });
     }
